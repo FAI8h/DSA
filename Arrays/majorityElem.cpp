ae0a6e62b3b4ac5a0cff649b5481670d5697842f
FAI8h <sazaralam20074@gmail.com>
2026-07-27 11:39:13 +0530

    Refactor majorityElem function to implement Boyer-Moore Voting Algorithm for improved efficiency

--- Arrays/majorityElem.cpp
+++ Arrays/majorityElem.cpp
@@ -6,16 +6,20 @@
 using namespace  std;
 
 /*
+
+! 1.
 ? this works but this is a bad approch ---
-? sorting is expensive ---  TC :- O(n log n), SC :- O(1)
+? sorting is expensive ---  TC :- O(n log n), SC :- O(log n)
 
 int majorityElem(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         return nums[nums.size()/2];
     }
-*/
+    
 
-
+! 2.
+? this is better approch --------
+? TC :- O(n), SC :- O(n)
 int majorityElem(vector<int>& nums){
     unordered_map<int, int> m;
     int n = nums.size();
@@ -28,6 +32,32 @@ int majorityElem(vector<int>& nums){
 
     return -1;
 }
+
+! 3.
+? this is best approch
+? Boyer-Moore Voting Algorithm
+? TC :- O(n), SC :- O(1)
+
+*/
+int majorityElem(vector<int>& nums){
+    int candidate = 0;
+    int count = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++){
+        if(count == 0){//? this means no candidate assign one
+            candidate = nums[i];
+        };
+        if(nums[i] == candidate){
+            count++;
+        }else{
+            count--;
+        };
+    };
+
+    return candidate;
+}
+
 
 int main () {
     vector<int> nums = {2,2,1,1,1,2,2};
