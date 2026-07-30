0344385c59bf34f439ad98fd1b3afd6250b0d21f
FAI8h <sazaralam20074@gmail.com>
2026-07-30 19:16:16 +0530

    solve later

---
+++ Arrays/bitmanupulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace  std;
+
+void clearBits(int num, int from, int to){
+    int st = from;
+    int end = to;
+
+    int numberMask;
+    for (int i = st; i < end; i++){
+        // numberMask = 
+    }
+}
+
+int main () {
+    // clearBits(15, 1, 3);
+    cout << (4 % 5) << endl;
+
+    return 0;
+}
