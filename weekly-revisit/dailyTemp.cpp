924172198ea87ca97c0c1bb2ecc75b7850b744f4
FAI8h <sazaralam20074@gmail.com>
2026-08-05 09:56:05 +0530

    weekly-revisit : daily Temperature

---
+++ weekly-revisit/dailyTemp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+
+using namespace  std;
+
+vector<int> dailyTemp(vector<int> &temps){
+    vector<int> ans(temps.size(),-1);
+    unordered_map<int, int> m;
+    stack<int> s;
+
+    for (int i = 0; i < temps.size(); i++){
+        while(!s.empty() && temps[i] > temps[s.top()]){
+            int prevDay = s.top();
+            s.pop();
+            ans[prevDay] = i - prevDay;
+        }
+        s.push(i);
+    }
+
+    return ans;
+}
+
+int main () {
+    vector<int> temp = {73,74,75,71,69,72,76,73};
+
+    vector<int> ans = dailyTemp(temp);
+    for(int num : ans){
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
