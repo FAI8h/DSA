3e923497a9f7b09637ea4c6f8489946f865f7d00
FAI8h <sazaralam20074@gmail.com>
2026-08-07 15:06:29 +0530

    string : valid pallindrome

---
+++ strings/isPallindrome.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+
+using namespace  std;
+
+/*
+bool validPallindrome(string s){
+    int left = 0, right = s.length() - 1;
+
+    while(left <= right){
+
+        if(!isalnum(s[left])){
+            left++;
+            continue;
+        }
+
+        if(!isalnum(s[right])){
+            right--;
+            continue;
+        }
+
+        if(tolower(s[left]) != tolower(s[right])){
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+*/
+
+bool validPallindrome(string s){
+    int left = 0, right = s.length() - 1;
+
+    while(left <= right){
+
+        if(!isalnum(s[left])){
+            left++;
+            continue;
+        }
+
+        if(!isalnum(s[right])){
+            right--;
+            continue;
+        }
+
+        if(tolower(s[left]) != tolower(s[right])){
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main () {
+    string s = "A man, a plan, a canal: Panama";
+    cout << validPallindrome(s)<<endl;
+    return 0;
+}
