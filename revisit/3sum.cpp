c3d49b3221f658cf8df059b8c3dfe55d1edef4db
FAI8h <sazaralam20074@gmail.com>
2026-08-10 07:24:54 +0530

    revisit : 3Sum

---
+++ revisit/3sum.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace  std;
+
+vector<vector<int>> threeSum(vector<int> & nums){
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int left = 0, right = 0;
+    vector<vector<int>> ans;
+    for (int i = 0; i < n; i++){
+        if(i > 0 && nums[i] == nums[i - 1]) continue;
+        int tar = nums[i];
+        left = i + 1;
+        right = n - 1;
+
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if (sum == -tar){
+                ans.push_back({tar, nums[left], nums[right]});
+                while(left < right && nums[left] == nums[left + 1]) left++;
+                while(left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+                continue;
+            }
+
+            if(sum > -tar){
+                right--;
+            }else{
+                left++;
+            }
+        }
+
+    }
+    return ans;
+}
+
+int main () {
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ans = threeSum(nums);
+
+    for(auto i : ans){
+        cout << "[ ";
+        for (int num : i)
+        {
+            cout << num << ", ";
+        }
+        cout << "]" << endl;
+    }
+    return 0;
+}
