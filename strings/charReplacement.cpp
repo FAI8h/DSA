d6aa0db0e0794ea1a03dd1023b6ced15dfcf6e31
FAI8h <sazaralam20074@gmail.com>
2026-08-11 13:36:27 +0530

    String : Approch 2 , indexed array

--- strings/charReplacement.cpp
+++ strings/charReplacement.cpp
@@ -6,13 +6,13 @@
 
 using namespace  std;
 
-//? leetCode Qno. 424
-
+/*
+! 1. 
 int replaceChar(string s, int k){
     int n = s.size();
     int maxWindow = 0, maxFrq = 0;
     int left = 0, right = 0;
-    unordered_map<char, int> m;
+    unordered_map<char,int> m;
     for (int i = 0; i < n; i++){
         if(m.find(s[right]) != m.end()){
             m[s[right]]++;
@@ -23,6 +23,32 @@ int replaceChar(string s, int k){
         
         if((right - left + 1) - maxFrq > k){
             m[s[left]]--;
+            left++;
+        }
+        maxWindow = right - left + 1;
+        right++;
+    }
+    return maxWindow;
+}
+*/
+
+//? leetCode Qno. 424
+
+int replaceChar(string s, int k){
+    int n = s.size();
+    int maxWindow = 0, maxFrq = 0;
+    int left = 0, right = 0;
+    int m[256] = {0};
+    for (int i = 0; i < n; i++){
+        if(m[s[right] - 'A']){
+            m[s[right] - 'A'] += 1;
+        }else{
+            m[s[right] - 'A'] = 1;
+        }
+        maxFrq = max(maxFrq, m[s[right] - 'A']);
+        
+        if((right - left + 1) - maxFrq > k){
+            m[s[left] - 'A']--;
             left++;
         }
         maxWindow = right - left + 1;
