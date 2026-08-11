bbb6cb683399b220446e6bca973a5732778d2ba3
FAI8h <sazaralam20074@gmail.com>
2026-08-11 06:55:32 +0530

    revisit : Sort Color

---
+++ revisit/sortColor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace  std;
+
+//? leetCode Qno. 75
+
+void DNF(vector<int> &nums){
+    int low = 0, mid = 0, high = nums.size() - 1;
+    
+    while(mid <= high){
+        if(nums[mid] == 0){
+            swap(nums[mid], nums[low]);
+            low++;
+            mid++;
+        }else if(nums[mid] == 1){
+            mid++;
+        }else{
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main () {
+    vector<int> nums = {2,1,0,1,2,0};
+    DNF(nums);
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
