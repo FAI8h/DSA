a60dce4305aa6e6c1a2fd131e91defdfeda7b9ed
FAI8h <sazaralam20074@gmail.com>
2026-09-11 08:42:18 +0530

    recursion : computing power optmised to O(log n)

--- recursion/0/power.cpp
+++ recursion/0/power.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 using namespace  std;
-
+/*
 int power(int x, int n){
     if(n == 0) return x;
     if(n == 1) return x;
 
     return x * (power(x, n - 1));
+}
+
+*/
+
+int power(int x, int n){
+    if(n == 0) return 1;
+    int half = power(x, n / 2);
+
+    return n % 2 ? half * half * x : half * half;
 }
 
 int main () {
