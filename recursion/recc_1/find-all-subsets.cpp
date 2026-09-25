6da031f2d4f8121ec9dae27b1b2ff6e8bf28453a
FAI8h <sazaralam20074@gmail.com>
2026-09-25 11:22:34 +0530

    recursion : find all sub sets through vector container

--- recursion/recc_1/find-all-subsets.cpp
+++ recursion/recc_1/find-all-subsets.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace  std;
 
 void subSets(string s, string ans){
@@ -13,8 +13,25 @@ void subSets(string s, string ans){
     subSets(s.substr(1, s.length() - 1), ans);
 }
 
+void subSets(vector<int>& v, vector<int>& ans, int i){
+    if(i == v.size()){
+        for(auto it : ans){
+            cout << "[ " << it << " ]";
+        }
+        cout << endl;
+        return;
+    }
+
+    ans.push_back(v[i]);
+    subSets(v, ans, i + 1);
+    ans.pop_back();
+    subSets(v, ans, i + 1);
+}
+
 int main () {
-    subSets("12345", "");
+    vector<int> v{1, 2, 3};
+    vector<int> ans;
+    subSets(v,ans, 0);
 
     return 0;
 }
